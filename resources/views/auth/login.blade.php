3992cf0301c12e146fb979a6bdc6c46068fcf58a
RusanMaharjan <rusanmhz098@gmail.com>
2022-02-08 13:16:59 +0545

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
@@ -45,4 +45,52 @@
             </div>
         </form>
     </x-jet-authentication-card>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+@extends('layouts.main')
+@section('pageTitle', 'Login')
+@section('content')
+
+    <x-guest-layout>
+        <!-- Login Page -->
+        <div class="signup pt-5">
+            <div class="fs-2 text-center mt-5">Login</div>
+            <hr style="width: 20%; margin: auto; color: #000; height: 0.5px" class="mb-5" />
+            <x-jet-validation-errors class="mb-4" />
+            <div class="container signup-container">
+                <form action="" method="post" class="pb-4">
+                    <div class="heading fs-3">Login to your account</div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter email.."
+                            required />
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="exampleFormControlInput1"
+                            placeholder="Enter Password.." required />
+                    </div>
+
+                    <div class="button text-center">
+                        <button type="submit" class="btn btn-success signup-btn">
+                            Login &rarr;
+                        </button>
+                    </div>
+                    <input type="checkbox" name="remember"> Remember me
+
+                    <div class="row">
+                        <div class="col-md-8">
+                            Don't have an account?<a class="login-text text-decoration-underline"
+                                href="signup.html">Register</a>
+                        </div>
+                        <div class="col-md-4">
+                            <a href="#" class="text-decoration-underline login-text">Forgot Password?</a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </x-guest-layout>
+
+@endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
@@ -57,4 +57,62 @@
             </div>
         </form>
     </x-jet-authentication-card>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+
+@extends('layouts.main')
+@section('pageTitle', 'Register')
+@section('content')
+
+    <x-guest-layout>
+        <!-- Signup Page -->
+        <div class="signup pt-5">
+            <div class="fs-2 text-center mt-5">Create An Account</div>
+            <hr style="width: 20%; margin: auto; color: #000; height: 3px" class="mb-5" />
+            <x-jet-validation-errors class="mb-4" />
+            <div class="container signup-container">
+                <form action="" method="post" class="pb-4">
+                    <div class="heading fs-3">Personal Information</div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter name.."
+                            required />
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Enter email.."
+                            required />
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Phone</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1"
+                            placeholder="Enter phone number.." required />
+                    </div>
+
+                    <div class="heading fs-3">Login Information</div>
+                    <div class="password-field align-items-center">
+                        <div class="mb-3 password">
+                            <label for="exampleFormControlInput1" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="exampleFormControlInput1"
+                                placeholder="Enter Password.." required />
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Confirm Password</label>
+                            <input type="password" class="form-control" id="exampleFormControlInput1"
+                                placeholder="Conrifm Password.." required />
+                        </div>
+                    </div>
+                    <div class="button text-center">
+                        <button type="submit" class="btn btn-success signup-btn">
+                            Signup &rarr;
+                        </button>
+                    </div>
+                    <div>
+                        Already have an account?<a class="login-text" href="login.html ">login</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </x-guest-layout>
+
+@endsection
