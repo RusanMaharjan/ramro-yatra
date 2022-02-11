6ba2daf4d3a28e419b318083e7fb33722da5ebf8
RusanMaharjan <rusanmhz098@gmail.com>
2022-02-11 12:42:55 +0545

    commit

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -2,15 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Route;
 use Illuminate\Http\Request;
 
 class RouteController extends Controller
 {
+    //function to display route page
     public function addRoute() {
         return view('admin.route.addRoute');
     }
 
+    //function for adding route in database
+    public function createRoute(Request $request) {
+        $request->validate([
+            'from' => 'required',
+            'to' => 'required'
+        ]);
+
+        $route = new Route();
+        $route->from = $request->from;
+        $route->to = $request->to;
+        $route->save();
+        return back()->with('message','Route added successfully.');
+    }
+
     public function getRoute() {
+        // $routes = Route::orderBy('route_id', 'ASC')->get();
         return view('admin.route.allRoute');
     }
 
