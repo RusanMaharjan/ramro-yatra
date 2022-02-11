9308ccadb5e981f592ad97deee67ba9c784827a5
RusanMaharjan <rusanmhz098@gmail.com>
2022-02-11 17:08:33 +0545

    commit

--- app/Http/Controllers/BusController.php
+++ app/Http/Controllers/BusController.php
@@ -2,12 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Bus;
+use App\Models\Operator;
+use App\Models\Route;
 use Illuminate\Http\Request;
 
 class BusController extends Controller
 {
+    //function to display Bus page
     public function addBus() {
-        return view('admin.bus.addBus');
+        $routes = Route::all();
+        $operators = Operator::all();
+        return view('admin.bus.addBus',compact('routes','operators'));
+    }
+
+    //function for adding bus data in database
+    public function createBus(Request $request) {
+        $request->validate([
+            'bus_name' => 'required',
+            'facilities' => 'required',
+            'time' => 'required',
+            'bus_number' => 'required',
+            'date' => 'required',
+            'price' => 'required',
+            'driver_name' => 'required',
+            'seat' => 'required',
+            'route_id' => 'required',
+            'operator_id' => 'required',
+            'img' => 'required'
+        ]);
+        $buses = new Bus();
+        $buses->bus_name = $request->bus_name;
+        $buses->facilities = $request->facilities;
+        $buses->time = $request->time;
+        $buses->date = $request->date;
+        $buses->price = $request->price;
+        $buses->driver_name = $request->driver_name;
+        $buses->bus_number = $request->bus_number;
+        $buses->seat = $request->seat;
+        $buses->route_id = $request->route_id;
+        $buses->operator_id = $request->operator_id;
+
+        $img = $request->file;
+        $imageName = time().'.'.$img->getClientoriginalExtension();
+        $request->file->move('img',$imageName);
+        $buses->img = $imageName;
+
+        $buses->save();
+        return back()->with('message', 'Bus added successfully;');
+
     }
 
     public function getBus() {
--- resources/views/admin/bus/addBus.blade.php
+++ resources/views/admin/bus/addBus.blade.php
@@ -14,132 +14,78 @@
                         Create Bus
                     </div>
                     <a href="{{ route('admin.getBus') }}" class="admin-a-btn btn btn-success">All Bus &rarr;</a>
-                </div>  
+                </div>
                 <hr style="color: #000; height: 3px" class="mb-5" />
-                
+
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Bus Name</label>
-                    <input
-                    type="text"
-                    class="form-control"
-                    id="exampleFormControlInput1"
-                    placeholder="Bus Name"
-                    name=""
-                    required
-                    />
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Bus Name" name=""
+                        required />
                 </div>
 
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Facilities</label>
-                    <input
-                    type="text"
-                    class="form-control"
-                    id="exampleFormControlInput1"
-                    placeholder="Facilities"
-                    name=""
-                    required
-                    />
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Facilities" name=""
+                        required />
                 </div>
 
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Location</label>
-                    <select class="form-select" name="" aria-label="Default select example">
+                    <select class="form-select" name="route_id" aria-label="Default select example">
                         <option selected>----- Select Locations -----</option>
-                        <option value="1">Kathmandu-Chitwan</option>
-                        <option value="2">Hetauda-Pokhara</option>
-                        <option value="3">Kathmandu-Pokhara</option>
-                      </select>
+                        @foreach ($routes as $route)
+                            <option value="{{ $route->route_id }}">{{ $route->from }} - {{ $route->to }}</option>
+                        @endforeach
+                    </select>
                 </div>
 
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Operator</label>
-                    <select class="form-select" name="" aria-label="Default select example">
-                        <option selected>----- Select Operators  -----</option>
-                        <option value="1">Kathmandu Yatayat</option>
-                        <option value="2">Hetauda Yatayat</option>
-                        <option value="3">Kathmandu Fast Track</option>
-                      </select>
+                    <select class="form-select" name="operator_id" aria-label="Default select example">
+                        <option selected>----- Select Operators -----</option>
+                        @foreach ($operators as $operator)
+                            <option value="{{ $operator->operator_id }}">{{ $operator->operator_name }}</option>
+                        @endforeach
+                    </select>
                 </div>
 
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Time</label>
-                    <input
-                    type="text"
-                    class="form-control"
-                    id="exampleFormControlInput1"
-                    placeholder="Time"
-                    name=""
-                    required
-                    />
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Time" name=""
+                        required />
                 </div>
 
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Seat</label>
-                    <input
-                    type="text"
-                    class="form-control"
-                    id="exampleFormControlInput1"
-                    placeholder="Seat"
-                    required
-                    />
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Seat" required />
                 </div>
 
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Date</label>
-                    <input
-                    type="date"
-                    class="form-control"
-                    id="exampleFormControlInput1"
-                    name=""
-                    required
-                    />
+                    <input type="date" class="form-control" id="exampleFormControlInput1" name="" required />
                 </div>
 
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Price</label>
-                    <input
-                    type="text"
-                    class="form-control"
-                    id="exampleFormControlInput1"
-                    placeholder="Price"
-                    name=""
-                    required
-                    />
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Price" name=""
+                        required />
                 </div>
 
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Bus Image</label>
-                    <input
-                    type="file"
-                    class="form-control"
-                    id="exampleFormControlInput1"
-                    name=""
-                    required
-                    />
+                    <input type="file" class="form-control" id="exampleFormControlInput1" name="" required />
                 </div>
 
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Driver's Name</label>
-                    <input
-                    type="text"
-                    class="form-control"
-                    id="exampleFormControlInput1"
-                    placeholder="Driver Name"
-                    name=""
-                    required
-                    />
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Driver Name"
+                        name="" required />
                 </div>
 
                 <div class="mb-3">
                     <label for="exampleFormControlInput1" class="form-label">Bus Number</label>
-                    <input
-                    type="text"
-                    class="form-control"
-                    id="exampleFormControlInput1"
-                    placeholder="Bus Number"
-                    name=""
-                    required
-                    />
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Bus Number" name=""
+                        required />
                 </div>
                 <div class="btnAdmin">
                     <button type="submit" class="btn btn-success">Add Bus &rarr;</button>
