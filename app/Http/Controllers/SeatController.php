e7b63817546cce58f4c2d11aad28f01c7d2f166d
RusanMaharjan <rusanmhz098@gmail.com>
2022-02-12 18:04:42 +0545

    commit

--- app/Http/Controllers/SeatController.php
+++ app/Http/Controllers/SeatController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Seat;
 use Illuminate\Http\Request;
 
 class SeatController extends Controller
 {
-    //
+    public function createSeat(Request $request) {
+
+        $seats = new Seat();
+        $seats->selected_seat = $request->selected_seat;
+        $seats->total_price = $request->total_price;
+        $seats->bus_id = $request->bus_id;
+        $seats->save();
+        return back()->with('message', 'Seat details added to your record successfully.');
+
+    }
 }
