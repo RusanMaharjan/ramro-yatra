76215b3f3c22281e5ab4509b715bc53b4426935a
RusanMaharjan <rusanmhz098@gmail.com>
2022-02-12 16:48:58 +0545

    commit

--- app/Http/Controllers/EditUserController.php
+++ app/Http/Controllers/EditUserController.php
@@ -7,16 +7,19 @@ use Illuminate\Http\Request;
 
 class EditUserController extends Controller
 {
+    //function for getting all users
     public function getUsers() {
         $users = User::orderBy('id', 'Asc')->get();
         return view('admin.users.allUser', compact('users'));
     }
 
+    //function for editing user
     public function editUser($id) {
         $user = User::find($id);
         return view('admin.users.updateUserRole', compact('user'));
     }
 
+    //function for updating user data
     public function updateUser(Request $request) {
         $user = User::find($request->id);
         $user->name = $request->name;
