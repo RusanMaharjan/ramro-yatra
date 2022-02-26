fd1cf782ffc84e913a33127ee3f0d9a02ed39290
RusanMaharjan <rusanmhz098@gmail.com>
2022-02-26 12:06:24 +0545

    commit

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-// Auth::routes();
-
 //User Routes
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/search-bus', [HomeController::class, 'searchBus'])->name('searchBus');
